71c2145fc4ef268094dd392fd455b98751965a98
itsr11 <118625963+itsr11@users.noreply.github.com>
2025-11-18 11:45:26 +0700

    Refactor image processing and normalization in main.py
    Fixed error in input array type and adjusted normalization dimensions.

--- main.py
+++ main.py
@@ -19,18 +19,21 @@ async def predict(file: UploadFile = File(...)):
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
     image = image.resize((256, 256))
 
-    arr = np.array(image).astype("float32") / 255.0
-    arr = np.transpose(arr, (2, 0, 1))  # HWC → CHW
+arr = np.array(image).astype("float32") / 255.0
+arr = np.transpose(arr, (2, 0, 1))
+arr = np.expand_dims(arr, 0)
 
-    # ---- Normalisasi MobileViT (WAJIB) ----
-    mean = np.array([0.485, 0.456, 0.406]).reshape(3,1,1)
-    std  = np.array([0.229, 0.224, 0.225]).reshape(3,1,1)
-    arr = (arr - mean) / std
-    # ---------------------------------------
+# Normalisasi MobileViT
+mean = np.array([0.485, 0.456, 0.406]).reshape(1,3,1,1)
+std  = np.array([0.229, 0.224, 0.225]).reshape(1,3,1,1)
+arr = (arr - mean) / std
 
-    arr = np.expand_dims(arr, 0)
+# ==== FIX ERROR di sini ====
+arr = arr.astype(np.float32)
+# ===========================
 
-    inputs = {session.get_inputs()[0].name: arr}
-    outputs = session.run(None, inputs)
+inputs = {session.get_inputs()[0].name: arr}
+outputs = session.run(None, inputs))
 
     return {"result": outputs[0].tolist()}
+
