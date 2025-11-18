c5fa84a2d46b887a7f285a9717e3f6a5cb5887e6
itsr11 <118625963+itsr11@users.noreply.github.com>
2025-11-18 11:42:13 +0700

    Implement input normalization for MobileViT model
    Added normalization step for MobileViT input.

--- main.py
+++ main.py
@@ -15,14 +15,20 @@ def home():
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    # Read & preprocess image
     image_bytes = await file.read()
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
-    image = image.resize((256, 256))  # Sesuaikan dgn input model
+    image = image.resize((256, 256))
 
     arr = np.array(image).astype("float32") / 255.0
     arr = np.transpose(arr, (2, 0, 1))  # HWC → CHW
-    arr = np.expand_dims(arr, 0)        # Add batch dim
+
+    # ---- Normalisasi MobileViT (WAJIB) ----
+    mean = np.array([0.485, 0.456, 0.406]).reshape(3,1,1)
+    std  = np.array([0.229, 0.224, 0.225]).reshape(3,1,1)
+    arr = (arr - mean) / std
+    # ---------------------------------------
+
+    arr = np.expand_dims(arr, 0)
 
     inputs = {session.get_inputs()[0].name: arr}
     outputs = session.run(None, inputs)
